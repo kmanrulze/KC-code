06bf3d3432d8bd37cb47f50528e92dfc24ae7a95
kmanrulze <kcguzman@comcast.net>
2019-10-02 11:30:18 -0600

    rectangle

---
+++ ShapesApp/ShapesApp.App/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ShapesApp.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length;
+            string input;
+            do
+            {
+                Console.WriteLine("Enter a length: ");
+                input = System.Console.ReadLine();
+            } while (!double.TryParse(input, out length));
+
+            double width;
+            
+        }
+    }
+}
---
+++ ShapesApp/ShapesApp.Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ShapesApp.Library
+{
+    public class Class1
+    {
+    }
+}
