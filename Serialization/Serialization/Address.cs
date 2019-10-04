7709ff1fe2162b2632c48c24c396424ffb86feed
kmanrulze <kcguzman@comcast.net>
2019-10-04 10:10:16 -0500

    Made folder project specific

--- Serialization/Serialization/Address.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Serialization
-{
-    public class Address
-    {
-        public string Street { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-    }
-}
