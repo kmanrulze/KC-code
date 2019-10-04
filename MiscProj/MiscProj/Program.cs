5063696d3f24d1eab3e3ac5a8f46f585e509900b
RAINBOOMERS-PC <kcguzman@comcast.net>
2019-10-04 09:38:24 -0500

    MiscProj added

---
+++ MiscProj/MiscProj/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MiscProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Typecsting
+            int five = 5;
+            double otherFive = five;
+            int nextFive = (int)otherFive;
+
+            // converstions when type heirarchies are concerned
+            var list = new List<int>();
+            object o = list; // implicit upcasting
+        }
+    }
+}
