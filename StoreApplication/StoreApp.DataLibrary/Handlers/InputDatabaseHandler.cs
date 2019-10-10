609267820ad4efcadf3d0620a7ed73f53f9d7bb4
RAINBOOMERS-PC <kcguzman@comcast.net>
2019-10-10 17:42:17 -0500

    Installed Microsoft.EntityFrameworkCore.Design and Microsoft.EntityFrameworkCore.SqlServer to all projects

--- StoreApplication/StoreApp.DataLibrary/Handlers/InputDatabaseHandler.cs
+++ StoreApplication/StoreApp.DataLibrary/Handlers/InputDatabaseHandler.cs
@@ -9,26 +9,7 @@ namespace StoreApp.DataAccess
         private string connectionString = File.ReadAllText(@"C:/revature/kc-project0/StoreApplication/StoreApp.DataLibrary/Connection String/string.txt");
         public void InputOrder(Order order)
         {
-            //Some code to add an order to a DB
-            var connection = new SqlConnection(connectionString);
-            var command = connection.CreateCommand();
 
-            //Command here
-            command.CommandText = @"INSERT
-            
-            ";
-            //command.Parameters.AddWithValue("@FirstName");
-            //command.Parameters.AddWithValue("@LastName");
-
-            connection.Open();
-
-            var reader = command.ExecuteReader();
-
-            while (reader.Read())
-            {
-                
-            }
-         
         }
         public void AddNewCustomerData(Customer cust)
         {
